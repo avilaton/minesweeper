b8bfd28655261ca571ae053d3b0b4519f7be211f
Gaston Avila <avila.gas@gmail.com>
2018-04-18 00:32:52 -0300

    Add boards endpoint for listing and creating boards

--- app.py
+++ app.py
@@ -4,11 +4,27 @@ from flask_restplus import Api, Resource
 api = Api()
 
 board = {
-    "id": 1,
+    "id": 2,
     "dimensions": [6, 6],
     "mines": [(1, 1), (4, 5), (1, 3)],
     "cells": [{"position": [0, 0], "value": 0}],
+    "over": False,
 }
+
+
+@api.route("/boards")
+class BoardsAPI(Resource):
+
+    def get(self):
+        return [
+            {"id": 1, "over": True},
+            {"id": 2, "over": False},
+            {"id": 3, "over": False},
+            {"id": 4, "over": False},
+        ]
+
+    def post(self):
+        return board, 201
 
 
 app = Flask(__name__)
