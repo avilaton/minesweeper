3fb187d0796cdd61d5f08593d5483f8f3710b102
Gaston Avila <avila.gas@gmail.com>
2018-04-18 00:29:15 -0300

    Add basic idea of what a board will be represented by

--- app.py
+++ app.py
@@ -3,5 +3,13 @@ from flask_restplus import Api, Resource
 
 api = Api()
 
+board = {
+    "id": 1,
+    "dimensions": [6, 6],
+    "mines": [(1, 1), (4, 5), (1, 3)],
+    "cells": [{"position": [0, 0], "value": 0}],
+}
+
+
 app = Flask(__name__)
 api.init_app(app)
