85b52926189ed81a3cc0f2e9c62f8aec654888a4
Gaston Avila <avila.gas@gmail.com>
2018-04-19 12:11:42 -0300

    Add db commands

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 import os
+import click
 from flask import Flask
 from app.api import api
 from app.database import db
@@ -12,4 +13,18 @@ def create_app():
 
     db.init_app(app)
     api.init_app(app)
+
+    @app.cli.command()
+    def initdb():
+        """Initialize the database."""
+        click.echo('Init the db')
+        db.create_all()
+
+
+    @app.cli.command()
+    def dropdb():
+        """Drop the database."""
+        click.echo('Drop the db')
+        db.drop_all()
+
     return app
